f7aea338207b1b735edeabc0f77947e9d63d9294
Jean-Baptiste Boric <jblbeurope@gmail.com>
2017-09-07 17:03:10 +0200

    Add "Hello, World!" text

--- hello_view.cpp
+++ hello_view.cpp
@@ -5,30 +5,35 @@ namespace helloworld {
 
 HelloView::HelloView() :
   View(),
-  m_color(0)
+  m_bufferTextView(KDText::FontSize::Large, 0.5, 0.5, KDColorBlack),
+  m_color(3),
+  m_kdcolor(Palette::GreyWhite)
 {
+  m_bufferTextView.setText("Hello, World!");
+  m_bufferTextView.setFrame(KDRect(0, 0, bounds().width(), bounds().height()));
 }
 
 void HelloView::drawRect(KDContext * ctx, KDRect rect) const {
-  KDColor color;
-  switch (m_color) {
-    case 0:
-      color = Palette::GreyDark;
-      break;
-    case 1:
-      color = Palette::GreyMiddle;
-      break;
-    case 2:
-      color = Palette::GreyBright;
-      break;
-    default:
-      color = Palette::GreyWhite;
-  }
-
-  ctx->fillRect(KDRect(0, 0, bounds().width(), bounds().height()), color);
+  ctx->fillRect(KDRect(0, 0, bounds().width(), bounds().height()), m_kdcolor);
 }
 
 void HelloView::reload() {
+  switch (m_color) {
+    case 0:
+      m_kdcolor = Palette::GreyDark;
+      break;
+    case 1:
+      m_kdcolor = Palette::GreyMiddle;
+      break;
+    case 2:
+      m_kdcolor = Palette::GreyBright;
+      break;
+    default:
+      m_kdcolor = Palette::GreyWhite;
+  }
+
+  m_bufferTextView.setBackgroundColor(m_kdcolor);
+
   markRectAsDirty(bounds());
 }
 
@@ -38,6 +43,18 @@ void HelloView::changeColor() {
     m_color = 0;
 
   reload();
+}
+
+int HelloView::numberOfSubviews() const {
+  return 1;
+}
+
+View * HelloView::subviewAtIndex(int index) {
+  return &m_bufferTextView;
+}
+
+void HelloView::layoutSubviews() {
+  m_bufferTextView.setFrame(KDRect(0, 0, bounds().width(), bounds().height()));
 }
 
 }
--- hello_view.h
+++ hello_view.h
@@ -12,8 +12,13 @@ public:
   void drawRect(KDContext * ctx, KDRect rect) const override;
   void reload();
   void changeColor();
+  int numberOfSubviews() const override;
+  View * subviewAtIndex(int index) override;
 private:
+  void layoutSubviews() override;
+  BufferTextView m_bufferTextView;
   int m_color;
+  KDColor m_kdcolor;
 };
 
 }
